3f80104af5a5e93fe78fc6bc8f5978d2651d796c
Karl <me@karlstratos.com>
2017-07-20 18:39:36 -0500

    .

--- mention2vec.py
+++ mention2vec.py
@@ -573,7 +573,7 @@ if __name__ == "__main__":
                            help="%(default)s")
     argparser.add_argument("--drop", type=float, default=0.4,
                            help="%(default)f")
-    argparser.add_argument("--lrate", type=float, default=0.0004,
+    argparser.add_argument("--lrate", type=float, default=0.0005,
                            help="%(default)f")
     argparser.add_argument("--dynet-mem")
     argparser.add_argument("--dynet-seed")
